b47dd4545909017d7cee67d6ea4e0cb45e8b6ef4
Han Ye Htun <HanYeHtun@fusionsol.com>
2023-11-07 10:35:33 +0630

    json server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 app.use(cors())
+app.use(express.json())
 
 app.use('/api/users', (req, res) => {
     return res.status(200).json({ message: 'good' })
