0cac44fe88b0bcc162388e76154a3cac3cc9fff7
Han Ye Htun <HanYeHtun@fusionsol.com>
2023-11-07 10:37:39 +0630

    json server

--- server/index.js
+++ server/index.js
@@ -3,6 +3,9 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.get("/", (req, res) => {
+    res.send("Express on Vercel");
+});
 
 app.use('/api/users', (req, res) => {
     return res.status(200).json({ message: 'good' })
